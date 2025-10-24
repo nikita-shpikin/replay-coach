67a08e23a27b35f11b6a519359736070a3315b41
Nikita Shpikin <72815770+nikita-shpikin@users.noreply.github.com>
2025-10-24 20:21:56 +0500

    feat(stage-5): breaks and exercises (add session counter, long break every 3rd start, random exercise slider with fade animations) (#6)
    * style(stage-3): исправлена адаптивность .setting__subtitle и размер шрифта кнопок
    
    * style(stage-3): исправлена расположение между кнопок align-items на stretch
    
    * style(stage-3): исправил .setting__subtitle добавил flex для прижатия к низу
    
    * style(stage-3): изменил стрктуру кнопок index.html и стили style.css для блоков с кнопками
    
    * feat(stage-3):  recommended topic (add eslintrc, eslintignore, random selection of topics relative to their completions)
    
    * style(stage-3): поправил ползунок в переключателе
    
    * fix(stage-3): удалил subTitleList из-за изменённой структуры
    
    * chore(stage-3): Изменил конфигурацию eslint
    
    * feat(stage-4): pomodoro timer (smooth scrolling, timer start, subtask display, subtask copying on click)
    
    * feat(stage-5): added an array of exercises and a catalog of assets
    
    * feat(stage-5): создал функцию работы перерывова
    
    * Реализовал пояявление слайдеров из .png, .gif, способы переключения слайдера клавиатурой, мышкой и тач
    
    * style(stage-5): добавлены стили для слайдов
    
    * style(stage-5): обновлены стили отображения картинок
    
    * fix(stage-5): добавлен passive:true в обработчик touchmove
    
    * chore(stage-5): сжаты и обрезаны изображения
    
    * fix(stage-5): добавлен passive:true в обработчик touchstart
    
    * style(stage-5): добавлена анимация заглушки и размеры для картинок
    
    * feat(stage-5): добавлена логика длинного перерыва на каждый 3-й запуск

--- src/scripts/module/startPomodoroSession.js
+++ src/scripts/module/startPomodoroSession.js
@@ -1,3 +1,7 @@
+import { EXERCISES } from '../../data/exercises'
+import { store } from '../store'
+
+// Клик на кнопку
 import { store } from '../store'
 
 export function startPomodoroSession() {
@@ -6,6 +10,10 @@ export function startPomodoroSession() {
 	const setting = document.querySelector('.setting ')
 
 	startTheme?.addEventListener('click', () => {
+		let countStart = Number(sessionStorage.getItem('sessionCount')) || 0
+		countStart++
+		sessionStorage.setItem('sessionCount', countStart)
+
 		pomodoroBlock.classList.add('pomodoro--active')
 
 		document.querySelector('.pomodoro')?.scrollIntoView({
@@ -49,6 +57,8 @@ function startPomodoroTimer() {
 		if (remainingTime <= 0) {
 			clearInterval(intervalId)
 
+			setTimeout(() => {
+				startBreakTimer()
 			setting.classList.remove('setting--inactive')
 
 			setting?.scrollIntoView({
@@ -71,6 +81,7 @@ function startPomodoroTimer() {
 		).padStart(2, '0')}`
 	}, 100)
 
+	// Вывод подайтемов в блоке помодоро
 	setTimeout(() => {
 		const pomodoroContent = document.querySelector('.pomodoro__content')
 
@@ -91,6 +102,15 @@ function startPomodoroTimer() {
 					.writeText(li.textContent)
 					.then(() => {
 						// Оповещение
+						console.log(
+							`%cТЕМА:%c ${store.getState().title}\n%cПОДТЕМЫ:%c ${
+								li.textContent
+							}`,
+							'color: #4CAF50; font-weight: bold;',
+							'color: inherit;',
+							'color: #2196F3; font-weight: bold;',
+							'color: inherit;'
+						)
 						console.log('Скопировано:', li.textContent)
 
 						// Стиль при клике
@@ -103,6 +123,156 @@ function startPomodoroTimer() {
 			pomodoroContent.appendChild(li)
 		})
 	}, 500)
+}
+
+function startBreakTimer() {
+	const pomodoroTitle = document.querySelector('.pomodoro__title')
+	const setting = document.querySelector('.setting ')
+	const pomodoroBlock = document.querySelector('.pomodoro')
+	const longBreak = parseInt(localStorage.getItem('long-duration'), 10)
+	const shortBreak = parseInt(localStorage.getItem('short-duration'), 10)
+
+	const durationLongBreak = longBreak * 60 * 1000
+	const durationShortBreak = shortBreak * 60 * 1000
+
+	const endLongBreak = Date.now() + durationLongBreak
+	const endShortBreak = Date.now() + durationShortBreak
+
+	resetPomodoroContent()
+
+	const intervalBreakId = setInterval(() => {
+		const remainingLongBreak = endLongBreak - Date.now()
+		const remainingShortBreak = endShortBreak - Date.now()
+
+		let countStart = Number(sessionStorage.getItem('sessionCount')) || 0
+		let remainingBreak
+
+		if (countStart % 3 === 0) {
+			remainingBreak = remainingLongBreak
+		} else {
+			remainingBreak = remainingShortBreak
+		}
+
+		// Обнуление
+		if (remainingBreak <= 0) {
+			clearInterval(intervalBreakId)
+
+			setting.classList.remove('setting--inactive')
+
+			setting?.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+
+			setTimeout(() => {
+				pomodoroBlock.classList.remove('pomodoro--active')
+			}, 500)
+
+			return
+		}
+
+		const min = Math.floor(remainingBreak / 60000)
+		const sec = Math.floor((remainingBreak % 60000) / 1000)
+
+		pomodoroTitle.textContent = `${String(min).padStart(2, '0')}:${String(
+			sec
+		).padStart(2, '0')}`
+	}, 100)
+
+	renderImageSlider()
+}
+
+function renderImageSlider() {
+	resetPomodoroContent()
+
+	const pomodoroContent = document.querySelector('.pomodoro__content')
+	const randomKey = getRandomKey(EXERCISES)
+	const exercises = EXERCISES[randomKey]
+	pomodoroContent.classList.add('pomodoro__content-exercises')
+
+	exercises.forEach(ex => {
+		const li = document.createElement('li')
+		li.classList.add('pomodoro__content-item')
+
+		const img = document.createElement('img')
+		img.classList.add('pomodoro__content-img')
+
+		img.alt = ex.title
+		img.title = ex.title
+		img.width = 100
+		img.height = 100
+
+		img.src = `./src/assets/exercises/${ex.src}`
+
+		li.appendChild(img)
+		pomodoroContent.appendChild(li)
+	})
+
+	let indexExercises = 0
+
+	function showSlide(i) {
+		const items = document.querySelectorAll('.pomodoro__content-item')
+
+		indexExercises = (i + items.length) % items.length
+
+		pomodoroContent.style.transform = `translateX(-${indexExercises * 80}%)`
+	}
+
+	document.addEventListener('keydown', e => {
+		if (e.key === 'ArrowRight') showSlide(indexExercises + 1)
+		if (e.key === 'ArrowLeft') showSlide(indexExercises - 1)
+	})
+
+	let startX = 0
+	let isDown = false
+
+	function onStart(e) {
+		isDown = true
+		startX = e.pageX || e.touches[0].pageX
+	}
+
+	function onMove(e) {
+		if (!isDown) return
+		const x = e.pageX || e.touches[0].pageX
+		const diff = x - startX
+
+		// если сдвиг больше 50px — листаем
+		if (diff > 50) {
+			showSlide(indexExercises - 1)
+			isDown = false
+		} else if (diff < -50) {
+			showSlide(indexExercises + 1)
+			isDown = false
+		}
+	}
+
+	function onEnd() {
+		isDown = false
+	}
+
+	pomodoroContent.addEventListener('mousedown', onStart)
+	pomodoroContent.addEventListener('mousemove', onMove)
+	pomodoroContent.addEventListener('mouseup', onEnd)
+	pomodoroContent.addEventListener('mouseleave', onEnd)
+
+	pomodoroContent.addEventListener('touchstart', onStart, { passive: true })
+	pomodoroContent.addEventListener('touchmove', onMove, { passive: true })
+	pomodoroContent.addEventListener('touchend', onEnd)
+}
+
+// Случайнный ключ объекта
+function getRandomKey(obj) {
+	const keys = Object.keys(obj)
+
+	const randomKey = Math.floor(Math.random() * keys.length)
+
+	return keys[randomKey]
+}
+
+// Очищение блока контента помодоро
+function resetPomodoroContent() {
+	const pomodoroContent = document.querySelector('.pomodoro__content')
+	pomodoroContent.innerHTML = ''
 }
 
 // 💡 Как это работает в цикле
